041ac7f993b7846787f4f055c08418b724763c0b
claytonstealth <claytonses@yahoo.com>
2022-06-01 09:40:22 -0600

    the rest

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,34 @@
 const prompt = require('prompt-sync')({ sigint: true });
+
+let n = Number(prompt("Enter a value for 'n': "));
+
+
+// 1st iteration
+// 0 1 1 2
+// p c t
+
+// 2nd iteration
+// 0 1 1 2
+//   p c t
+
+// 3rd iteration
+// 0 1 1 2 3
+//     p c t
+
+let prevNum = 0;
+let currentNum = 1;
+let total = 0; //preNum + currentNum;
+
+
+console.log(prevNum); //account for the first 0 in the sequence
+
+let count = 1; //start at 1 to accopunt for the first 0 in the sequence
+while (count < n) { //loop runs n times
+
+    console.log(currentNum);
+    total = prevNum + currentNum
+    prevNum = currentNum;
+    currentNum = total;
+
+    count++;
+}
--- number-summer.js
+++ number-summer.js
@@ -1,7 +1,9 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
-let numChoice = prompt("Enter some numbers,then type done when complete ");
-let sum = 0;
+
+// let input = prompt("> ") //variable for user inputted data
+let numChoice = prompt("Enter some numbers,then type 'done' when complete ");
+let sum = 0; //vairable for current total
 if (numChoice === "done") {
     console.log("that was quick")
 } else {
